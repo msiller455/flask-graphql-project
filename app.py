a8a62bda0e0f7fbd0af00c6f1cc81c43d54e059f
Michael <msiller455@gmail.com>
2020-07-01 11:46:40 -0700

    adds database set up as well as user and post models

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.debug = True
 
 # Configs
-# TO-DO
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 # Modules
-# TO-DO
+db = SQLAlchemy(app)
 
 # Models
-# TO-DO
+class User(db.Model):
+    __tablename__ = 'users'
+
+    uuid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256), index=True, unique=True)
+    posts = db.relationship('Post', backref='author')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    uuid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), index=True)
+    body = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.uuid'))
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
 
 # Schema Objects
 # TO-DO
