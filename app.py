23551f236b40142b0133a96135cf7c3ba9600630
Michael <msiller455@gmail.com>
2020-07-01 11:23:58 -0700

    finishes app.py setup

--- app.py
+++ app.py
@@ -2,3 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 app.debug = True
+
+# Configs
+# TO-DO
+
+# Modules
+# TO-DO
+
+# Models
+# TO-DO
+
+# Schema Objects
+# TO-DO
+
+# Routes
+# TO-DO
+
+@app.route('/')
+def index():
+    return '<p>Hello World</p>'
+
+if __name__ == '__main__':
+    app.run()
